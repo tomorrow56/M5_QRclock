6118d7d56b0226de98f518d5b707f9190063a107
ThousanDIY <tomorrow56@gmail.com>
2023-06-23 23:59:55 +0900

    Add files via upload

--- src/M5_QRclock/M5_QRclock.ino
+++ src/M5_QRclock/M5_QRclock.ino
@@ -1,15 +1,22 @@
 /***************************************************
- M5 QR clock
+ M5 QR clock with M5unified
  Copyright(c) @tomorrow56 All rights reserved.
 ***************************************************/
-#include <M5Stack.h>
-//#include "M5StackUpdater.h"
+#if defined ( ARDUINO )
+#include <Arduino.h>
+#include <SD.h>
+#include <SPIFFS.h>
+#endif
+
+// Include this to enable the M5 global instance.
+#include <M5Unified.h>      // https://github.com/m5stack/M5Unified
+#include <esp_log.h>
 
 #include <WiFi.h>
 #include <TimeLib.h>        // https://forum.arduino.cc/index.php?topic=415296.0
 
-const char* ssid       = "xxxxxxxx";  // your SSID
-const char* password   = "xxxxxxxx";  // your Password
+const char* ssid       = "xxxxxxxx";    // your SSID
+const char* password   = "xxxxxxxx";    // your password
 IPAddress ipadr;
 
 const char* ntpServer = "ntp.nict.jp";
@@ -22,45 +29,158 @@ int last_min = 0;
 int last_sec = 0;
 
 void setup(){ 
-  // init lcd, serial, sd card
-  M5.begin(true, true, true);
-  M5.Power.begin();
+  auto cfg = M5.config();
 
-/*
-  if(digitalRead(BUTTON_A_PIN) == 0){
-    Serial.println("Will Load menu binary");
-    updateFromFS(SD);
-    ESP.restart();
+#if defined ( ARDUINO )
+  cfg.serial_baudrate = 115200;   // default=115200. if "Serial" is not needed, set it to 0.
+#endif
+  cfg.clear_display = true;  // default=true. clear the screen when begin.
+  cfg.output_power  = false;  // default=true. use external port 5V output.
+  cfg.internal_imu  = false;  // default=true. use internal IMU.
+  cfg.internal_rtc  = true;  // default=true. use internal RTC.
+  cfg.internal_spk  = false;  // default=true. use internal speaker.
+  cfg.internal_mic  = false;  // default=true. use internal microphone.
+  cfg.external_imu  = false;  // default=false. use Unit Accel & Gyro.
+  cfg.external_rtc  = false;  // default=false. use Unit RTC.
+  cfg.led_brightness = 0;   // default= 0. system LED brightness (0=off / 255=max) (※ not NeoPixel)
+
+  // external speaker setting.
+  cfg.external_speaker.module_display = false;  // default=false. use ModuleDisplay AudioOutput
+  cfg.external_speaker.hat_spk        = false;  // default=false. use HAT SPK
+  cfg.external_speaker.hat_spk2       = false;  // default=false. use HAT SPK2
+  cfg.external_speaker.atomic_spk     = false;  // default=false. use ATOMIC SPK
+  cfg.external_speaker.module_rca     = false; // default=false. use ModuleRCA AudioOutput
+
+  // external display setting. (Pre-include required)
+  cfg.external_display.module_display = false;  // default=true. use ModuleDisplay
+  cfg.external_display.atom_display   = false;  // default=true. use AtomDisplay
+  cfg.external_display.unit_glass     = false;  // default=true. use UnitGLASS
+  cfg.external_display.unit_oled      = false;  // default=true. use UnitOLED
+  cfg.external_display.unit_lcd       = false;  // default=true. use UnitLCD
+  cfg.external_display.unit_rca       = false;  // default=true. use UnitRCA VideoOutput
+  cfg.external_display.module_rca     = false;  // default=true. use ModuleRCA VideoOutput
+
+  // begin M5Unified.
+  M5.begin(cfg);
+
+  M5.Display.setBrightness(128);
+  M5.Display.setCursor(0, M5.Display.height()/24);
+  M5.Display.setTextColor(WHITE);
+  M5.Display.setTextSize(2);
+
+  const char* name;
+
+  switch (M5.getBoard()){
+#if defined (CONFIG_IDF_TARGET_ESP32S3)
+  case m5::board_t::board_M5StackCoreS3:
+    name = "StackS3";
+    break;
+  case m5::board_t::board_M5AtomS3Lite:
+    name = "ATOMS3Lite";
+    break;
+  case m5::board_t::board_M5AtomS3:
+    name = "ATOMS3";
+    break;
+#elif defined (CONFIG_IDF_TARGET_ESP32C3)
+  case m5::board_t::board_M5StampC3:
+    name = "StampC3";
+    break;
+  case m5::board_t::board_M5StampC3U:
+    name = "StampC3U";
+    break;
+#else
+  case m5::board_t::board_M5Stack:
+    name = "Stack";
+    break;
+  case m5::board_t::board_M5StackCore2:
+    name = "StackCore2";
+    break;
+  case m5::board_t::board_M5StickC:
+    name = "StickC";
+    break;
+  case m5::board_t::board_M5StickCPlus:
+    name = "StickCPlus";
+    break;
+  case m5::board_t::board_M5StackCoreInk:
+    name = "CoreInk";
+    break;
+  case m5::board_t::board_M5Paper:
+    name = "Paper";
+    break;
+  case m5::board_t::board_M5Tough:
+    name = "Tough";
+    break;
+  case m5::board_t::board_M5Station:
+    name = "Station";
+    break;
+  case m5::board_t::board_M5Atom:
+    name = "ATOM";
+    break;
+  case m5::board_t::board_M5AtomPsram:
+    name = "ATOM PSRAM";
+    break;
+  case m5::board_t::board_M5AtomU:
+    name = "ATOM U";
+    break;
+  case m5::board_t::board_M5TimerCam:
+    name = "TimerCamera";
+    break;
+  case m5::board_t::board_M5StampPico:
+    name = "StampPico";
+    break;
+#endif
+  default:
+    name = "Who am I ?";
+    break;
   }
-*/
 
-  M5.Lcd.setBrightness(128);
-  M5.Lcd.setCursor(0, 0);
-  M5.Lcd.setTextColor(WHITE);
-  M5.Lcd.setTextSize(2);
+  M5.Display.startWrite();
+  M5.Display.print("Core:");
+  M5.Display.println(name);
+  ESP_LOGI("setup", "core:%s", name);
 
-  //Serial.begin(115200);
+  // run-time branch : imu model check
+  switch (M5.Imu.getType())
+  {
+  case m5::imu_t::imu_mpu6050:
+    name = "MPU6050";
+    break;
+  case m5::imu_t::imu_mpu6886:
+    name = "MPU6886";
+    break;
+  case m5::imu_t::imu_mpu9250:
+    name = "MPU9250";
+    break;
+  case m5::imu_t::imu_sh200q:
+    name = "SH200Q";
+    break;
+  default:
+    name = "none";
+    break;
+  }
+  M5.Display.print("IMU:");
+  M5.Display.println(name);
+//  M5.Display.endWrite();
+  ESP_LOGI("setup", "imu:%s", name);
+
   Serial.println("M5 QRclock");
-  M5.Lcd.println("M5 QRclock");
+  M5.Display.println("M5 QRclock");
 
-  M5.Lcd.print("Connect to " + (String)ssid);
+  M5.Display.print("Connect to " + (String)ssid);
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
       delay(500);
-      M5.Lcd.print(".");
+      M5.Display.print(".");
   }
-  M5.Lcd.println(" CONNECTED");
+  M5.Display.println(" CONNECTED");
 
-  M5.Lcd.setTextColor(GREEN);
-  M5.Lcd.println();
-  M5.Lcd.println("SSID:" + (String)WiFi.SSID());
+  M5.Display.setTextColor(GREEN);
+  M5.Display.println();
+  M5.Display.println("SSID:" + (String)WiFi.SSID());
   ipadr = WiFi.localIP();
-  M5.Lcd.println("IP adrs: " + (String)ipadr[0] + "." + (String)ipadr[1] + "." + (String)ipadr[2] + "." + (String)ipadr[3]);
-  M5.Lcd.println();
-
-  M5.Lcd.fillScreen(TFT_BLACK);
-  M5.Lcd.setCursor(10, 10);
+  M5.Display.println("IP adrs: " + (String)ipadr[0] + "." + (String)ipadr[1] + "." + (String)ipadr[2] + "." + (String)ipadr[3]);
+  M5.Display.println();
 
   //init and get the time
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
@@ -78,16 +198,16 @@ void setup(){
 
   for(int i = 0; i < retry; i++){
     if(!getLocalTime(&timeinfo)){
-      M5.Lcd.setTextColor(RED);
+      M5.Display.setTextColor(RED);
       Serial.println("Failed to obtain time");
-      M5.Lcd.println("Failed to obtain time");
+      M5.Display.println("Failed to obtain time");
       if(i == retry - 1){
         return;
       }
     }else{
-      M5.Lcd.setTextColor(GREEN);
+      M5.Display.setTextColor(GREEN);
       Serial.println("Connected to NTP Server!");
-      M5.Lcd.println("Connected to NTP Server!");
+      M5.Display.println("Connected to NTP Server!");
       break;
     }
   }
@@ -109,27 +229,19 @@ void setup(){
   setTime(hh, mm, ss, dd, mon, yy);
 
   Serial.printf("%04d-%02d-%02d %02d:%02d:%02d\n", yy, mon, dd, hh, mm, ss);
-  M5.Lcd.setTextColor(WHITE);
-  M5.Lcd.println();
-  M5.Lcd.printf("%04d-%02d-%02d %02d:%02d:%02d\n", yy, mon, dd, hh, mm, ss);
+  M5.Display.setTextColor(WHITE);
+  M5.Display.println();
+  M5.Display.printf("%04d-%02d-%02d %02d:%02d:%02d\n", yy, mon, dd, hh, mm, ss);
 
-//  Serial.printf("%02d:%02d:%02d\n", hh, mm, ss);
-//  M5.Lcd.printf("%02d:%02d:%02d\n", hh, mm, ss);
-
-  M5.Lcd.setTextColor(WHITE);
-
-  M5.Lcd.fillScreen(TFT_BLACK);
-  M5.Lcd.setBrightness(127);
+  M5.Display.fillScreen(TFT_BLACK);
 }
 
 void loop(){ 
   int i = 0; 
   String NowTime = "";
 
-  if (last_min != minute()){
-//  if (last_sec != second()){
-//    M5.Lcd.fillScreen(TFT_BLACK);
-    M5.Lcd.fillRect(60, 20, 200, 200, TFT_WHITE);
+//  if (last_min != minute()){
+  if (last_sec != second()){
 
     NowTime = String(year()) + "-";
 
@@ -137,7 +249,6 @@ void loop(){
     while ((i+1)*10 <= month()){
       i++;
     }
-
     if(i == 0){
       NowTime = NowTime + "0" + String(month()) + "-";
     }else{
@@ -148,7 +259,6 @@ void loop(){
     while ((i+1)*10 <= day()){
       i++;
     }
-
     if(i == 0){
       NowTime = NowTime + "0" + String(day()) + " ";
     }else{
@@ -159,7 +269,6 @@ void loop(){
     while ((i+1)*10 <= hour()){
       i++;
     }
-
     if(i == 0){
       NowTime = NowTime + "0" + String(hour()) + ":";
     }else{
@@ -170,7 +279,6 @@ void loop(){
     while ((i+1)*10 <= minute()){
       i++;
     }
-
     if(i == 0){
       NowTime = NowTime + "0" + String(minute()) + ":";
     }else{
@@ -181,7 +289,6 @@ void loop(){
     while ((i+1)*10 <= second()){
       i++;
     }
-
     if(i == 0){
       NowTime = NowTime + "0" + String(second());
     }else{
@@ -189,9 +296,36 @@ void loop(){
     }
 
     Serial.println(NowTime);
-    M5.Lcd.qrcode(NowTime, 70, 30, 180, 2);
+
+
+    switch (M5.getBoard()){
+      case m5::board_t::board_M5StackCoreS3:
+      case m5::board_t::board_M5Stack:
+      case m5::board_t::board_M5StackCore2:
+       // 320 x 240
+        M5.Display.fillRect(40, 0, 240, 240, TFT_WHITE);
+        M5.Display.qrcode(NowTime, 50, 10, 220, 2);
+        break;
+      case m5::board_t::board_M5AtomS3:
+        // 128 x 128
+        M5.Display.fillRect(0, 0, 128, 128, TFT_WHITE);
+        M5.Display.qrcode(NowTime, 1, 1, 126, 2);
+        break;
+      case m5::board_t::board_M5StickC:
+        // 80 x 160
+        M5.Display.fillRect(0, 40, 80, 80, TFT_WHITE);
+        M5.Display.qrcode(NowTime, 2, 42, 76, 2);
+        break;
+      case m5::board_t::board_M5StickCPlus:
+        // 135 x 240
+        M5.Display.fillRect(0, 52, 135, 135, TFT_WHITE);
+        M5.Display.qrcode(NowTime, 4, 56, 127, 2);
+        break;
+      default:
+        break;
+    }
 
     last_min = minute();
-//    last_sec = second();
+    last_sec = second();
   }
 } 
